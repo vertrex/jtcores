a6bea4da27b8a925b6f616cbe0f150287e1f1e58
Solal Jacob <jacob.solal@gmail.com>
2025-06-30 23:24:10 +0200

    FIX SG0140 ! palette shift is fixed !

--- cores/toki/hdl/sg0140.v
+++ cores/toki/hdl/sg0140.v
@@ -34,8 +34,7 @@ module sg0140(
 //scroll pos0 etc 
 //it certainly do other stuff like partial mixing
 //
-reg [7:0] char_latch;
-reg [7:0] bk_latch; 
+
 
 //always @(posedge clk) begin 
   //met pri a 1 si un des deux et differ de hf ?
@@ -51,20 +50,26 @@ reg [7:0] bk_latch;
 //  01   bk1 1 char 0 (8)   
 //  10   bk1 0 char 1 (4)
 //  11   bk1 1 char 1 (4) //char > bk1 so ok
+//
+reg [3:0] COL_A_LATCH;
+reg [3:0] COL_B_LATCH; 
+
 always @(posedge clk) begin 
-  //if (EN_A)
-    //bk_latch[7:0] <= {COL_A[3:0], PIC_A[3:0]};
-  //if (EN_B)
+  if (EN_B)
+     COL_B_LATCH[3:0] <= COL_B[3:0];
+
+  if (EN_A)
+     COL_A_LATCH[3:0] <= COL_A[3:0];
     //char_latch[7:0] <= {COL_B[3:0], PIC_B[3:0]};
 
   ON_A <= PIC_A[3:0] == 'hf ? 1'b0 : 1'b1;
   ON_B <= PIC_B[3:0] == 'hf ? 1'b0 : 1'b1;
 
-  Q[7:0] <= PIC_B[3:0] != 'hf ?  {COL_B[3:0], PIC_B[3:0]} :
-                                 {COL_A[3:0], PIC_A[3:0]};
+  Q[7:0] <= PIC_B[3:0] != 'hf ?  {COL_B_LATCH[3:0], PIC_B[3:0]} :
+                                 {COL_A_LATCH[3:0], PIC_A[3:0]};
 end
 
-//assign Q[7:0] = ON_A ?  {COL_B[3:0], PIC_B[3:0]} :
+//assign Q[7:0] = ON_B ?  {COL_B[3:0], PIC_B[3:0]} :
                         //{COL_A[3:0], PIC_A[3:0]};
 
 endmodule
